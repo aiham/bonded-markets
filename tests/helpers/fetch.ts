b3080b545bd39ea5169eea6ffd1ebc1bb8c9fb0a
Aiham Hammami <aiham@users.noreply.github.com>
2022-01-19 05:40:27 +1100

    Refactor fetchMarginalPrice and fetchWholePrice to use shared fetchCurveSupply()

--- tests/helpers/fetch.ts
+++ tests/helpers/fetch.ts
@@ -11,7 +11,7 @@ import { linearCurve, linearDefiniteIntegral } from "./curveMath";
 const provider = anchor.Provider.env();
 const program = anchor.workspace.BondedMarkets as Program<BondedMarkets>;
 
-export const fetchMarginalPrice = async (market: Market) => {
+const fetchCurveSupply = async (market: Market) => {
   let TargetMint = new Token(
     provider.connection,
     market.targetMint,
@@ -27,27 +27,18 @@ export const fetchMarginalPrice = async (market: Market) => {
       return market.amountBurned.toNumber();
     });
   let curveSupply = targetMintSupply + amountBurned;
+  return curveSupply;
+};
+
+export const fetchMarginalPrice = async (market: Market) => {
+  let curveSupply = await fetchCurveSupply(market);
   let price = linearCurve(curveSupply);
   console.log("marginal price——", market.name, "==", price);
   return price;
 };
 
 export const fetchWholePrice = async (market: Market) => {
-  let TargetMint = new Token(
-    provider.connection,
-    market.targetMint,
-    TOKEN_PROGRAM_ID,
-    web3.Keypair.generate()
-  );
-  let targetMintSupply = await TargetMint.getMintInfo().then((response) => {
-    return response.supply.toNumber();
-  });
-  let amountBurned = await program.account.market
-    .fetch(market.address)
-    .then((market) => {
-      return market.amountBurned.toNumber();
-    });
-  let curveSupply = targetMintSupply + amountBurned;
+  let curveSupply = await fetchCurveSupply(market);
   let price = linearDefiniteIntegral(
     curveSupply - 1 * TARGET_DECIMAL_MODIFIER,
     curveSupply
